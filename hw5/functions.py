2baab7ff68287c7a13d331632d20511b81d9df2f
seymota <seyed.m.t.t@gmail.com>
2023-01-09 19:00:17 +0330

    excercise 5.1.5 completed

--- hw5/functions.py
+++ hw5/functions.py
@@ -10,24 +10,19 @@ def gaussian_pyramid(image, n_levels=6):
     Compute the Gaussian pyramid
 
     Inputs:
-    - image: Input image of size (N,M)
-    - n_levels: Number of stages for the Gaussian pyramid
+        - image: Input image of size (N,M)
+        - n_levels: Number of stages for the Gaussian pyramid
 
     Returns:
-    desired gaussian pyramid 
+        desired gaussian pyramid 
     """
 
     # approximate length 5 Gaussian filter using binomial filter
-
     a = 0.4
     b = 1./4
     c = 1./4 - a/2
 
     filt =  np.array([[c, b, a, b, c]])
-
-    # approximate 2D Gaussian
-    # filt = convolve2d(filt, filt.T)
-
     pyr = [image]
 
     for i in np.arange(n_levels):
@@ -49,18 +44,18 @@ def laplacian_pyramid(gaussian_pyr):
     Compute the laplacian pyramid
 
     Inputs:
-    - gaussian_pyr: Input gaussian pyramid  
+        - gaussian_pyr: Input gaussian pyramid  
 
     Returns:
-    desired laplacian pyramid 
+        desired laplacian pyramid 
     """
 
     # details pyramid(laplacian)
     # first index should be, the last level of gaussian pyramid
+    # alogorithm bulid the pyramid by scaling up the last gaussian pyramid element
     laplacian_top = gaussian_pyr[-1]
     n_levels = len(gaussian_pyr) - 1
     
-
     laplacian_pyr = [laplacian_top]
     for i in range(n_levels,0,-1):
         # this is the size of gaussian level to be expanded
@@ -76,6 +71,21 @@ def laplacian_pyramid(gaussian_pyr):
     return laplacian_pyr
 
 def pyramid_reconstruct(gaussian_pyr):
+    """
+    reconstruct the original image, using provided gaussian (or approximation) pyramid
+    NOTE: its really simillar to 'laplacian_pyramid' function, just has one more step.
+    that is the summation step, where we add laplacian iamge and expanded gaussian
+    in order to build next level image in the pyramid
+
+
+    Inputs:
+        - gaussian_pyr: Input gaussian pyramid
+
+    Returns:
+        desired laplacian pyramid and reconstructed pyramid
+    """
+
+
     laplacian_top = gaussian_pyr[-1]
     n_levels = len(gaussian_pyr) - 1
     
@@ -91,41 +101,97 @@ def pyramid_reconstruct(gaussian_pyr):
         laplacian = np.subtract(gaussian_pyr[i-1], gaussian_expanded)
         laplacian_pyr.append(laplacian)
 
+        # laplacian and expanded gaussian, make next level iamge in reconstruction pyramid
         reconstruct = np.add(laplacian , gaussian_expanded)
         reconstruct_pyr.append(reconstruct)
 
     return laplacian_pyr , reconstruct_pyr
 
 def box_filter(image,windowSize=3,imagePaddingSize=0):
+    """
+    reconstruct the original image, using provided gaussian (or approximation) pyramid
+    NOTE: its really simillar to 'laplacian_pyramid' function, just has one more step.
+    that is the summation step, where we add laplacian iamge and expanded gaussian
+    in order to build next level image in the pyramid
+
+
+    Inputs:
+        - image: Input image of size (N,M)
+        - windowSize: size of averaging kernel
+        - imagePaddingSize: size of image padding. assumed to be eqaul in length and width
+
+    Returns:
+        smoothed image with (windowSize*windowSize) averaging kernel
+    """
+
     width = image.shape[0]
     length = image.shape[1]
     size = windowSize-1
+
+    # if input image has padding, we drop the padding.
+    # using 'uint8' to have pixels in range (0,255).
     newImage = np.zeros((width-(2*imagePaddingSize),length-(2*imagePaddingSize)),dtype='uint8')
 
     for i in range(0,length,1):
         for j in range(0,width,1):
+            # calculate proper boundaries for window
+            # in left and top edges, indexes should be greater than 0
             start = (max(j-size, 0),max(i-size, 0))
-
+            # in right and down edges, indexes should be less than image length and width
             end = (min(j+size, width-1),min(i+size, length-1))
 
+            # crop a part of image which fits to kernel window
             buffer = image.copy()[start[0]:(end[0]+1), start[1]:(end[1]+1)][0]
+
+            # averaging and replacing in the output image
             buffer_mean = np.mean(buffer)
             newImage[j,i] = buffer_mean
 
     return newImage
 
 def replication(image,zoom_factor=0.5):
+    '''
+    the nearest neighbor alogorithm, which is the simplest way to interpolate.
+    also, shrink image into size, quarter than the input image size.
+
+    Inputs:
+        - image: Inpute image of size (N,M)
+        - zoom_factor: 
+    '''
+    
+    # output image size calculation
     newWidth = math.floor(image.shape[1]*zoom_factor) 
     newLength = math.floor(image.shape[0]*zoom_factor) 
+
+    # new image using nearest neighbor method
     return cv2.resize(image ,(newWidth,newLength), interpolation=cv2.INTER_NEAREST)
 
 def approximation_pyramid(image , n_levels):
+    """
+    Compute the approximation pyramid (gaussian pyramid, but using
+    averaging kernel instead of gaussian kernel)
+
+    Inputs:
+        - image: Input image of size (N,M)
+        - n_levels: Number of stages for the approximation pyramid
+
+    Returns:
+        desired approximation pyramid 
+    """
+
+    # first level of pyramid is the original iamge
     level_zero = image.copy()
     approxi_pyr = [level_zero]
+
+
     for _ in range(n_levels):
+
+        # applying averaging filter
         level_zero = box_filter(level_zero,windowSize=2)
+
+        # applying replication for interpolation
         level_zero = replication(level_zero)
-        # print(level_zero.shape)
+
         approxi_pyr.append(np.asarray(level_zero))
     
     return approxi_pyr

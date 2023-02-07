c7d9b0fa90b8904cad428a2ee3bf6d5fef74148b
seymota <seyed.m.t.t@gmail.com>
2023-02-07 19:40:32 +0330

    harris improvement

--- hw7/functions.py
+++ hw7/functions.py
@@ -5,6 +5,56 @@ from tqdm.notebook import tqdm
 import matplotlib.pyplot as plt
 plt.style.use(plt.style.available[5])
 
+def harris_corner_detector(img, window_size=5, k=0.04, threshold=0.3):  
+    img_gaussian = cv2.GaussianBlur(img, (3, 3), 0)
+
+    height = img.shape[0]  # shape[0] outputs height.
+    
+
+    width = img.shape[1] # shape[1] outputs width.
+    matrix_R = np.zeros((height, width))
+
+    # Calculate the x e y image derivatives (dx e dy)
+    dx = cv2.Sobel(img_gaussian, cv2.CV_64F, 1, 0, ksize=3)
+    dy = cv2.Sobel(img_gaussian, cv2.CV_64F, 0, 1, ksize=3)
+    # dy, dx = np.gradient(gray)
+
+    # Calculate product and second derivatives (dx2, dy2 e dxy)
+    dx2 = np.square(dx)
+    dy2 = np.square(dy)
+    dxy = dx*dy
+
+    offset = int(window_size / 2)
+
+    # Calcular a soma dos produtos das derivadas para cada pixel (Sx2, Sy2 e Sxy)
+    for y in range(offset, height-offset):
+        for x in range(offset, width-offset):
+            Sx2 = np.sum(dx2[y-offset:y+1+offset, x-offset:x+1+offset])
+            Sy2 = np.sum(dy2[y-offset:y+1+offset, x-offset:x+1+offset])
+            Sxy = np.sum(dxy[y-offset:y+1+offset, x-offset:x+1+offset])
+
+            # Define the Second moment matrix M(x,y)=[[Sx2,Sxy],[Sxy,Sy2]]
+            M = np.array([[Sx2, Sxy], [Sxy, Sy2]])
+
+            # Calculate the response function ( R=det(H)-k(Trace(H))^2 )
+            det = np.linalg.det(M)
+            tr = np.matrix.trace(M)
+            R = det-k*(tr**2)
+            matrix_R[y-offset, x-offset] = R
+
+    # Apply a threshold
+    matrix = normalize(matrix_R.copy() ,1,0)
+
+    for y in range(offset, height-offset):
+        for x in range(offset, width-offset):
+            value = matrix[y, x]
+
+            if value > threshold:
+                cv2.circle(img, (x, y), 3, (0, 255, 0))
+
+    return img
+
+
 def BruteForceMatcher(src_desc,test_desc,threshold=0.75):
     """
     Apply a brute-force method to find matching interest points between the source and test description vectors.
@@ -271,57 +321,26 @@ def stitch(left, right, H):
     return stitch_image
 
 
-def harris_corner_detector(img, window_size=5, k=0.04, threshold=0.4):
-    # copy the image in an new array
-    # img = np.array(img)
 
-    # gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+def normalize(array,newMax,newMin):
+    """
+    A simple normalization function.
+
+    Inputs:
+        - array: Array to be normalized
+        - newMax: Max of new range.
+        - newMin: Min of new range.
+
+    Returns:
+        Normalized array.
     
-    img_gaussian = cv2.GaussianBlur(img, (3, 3), 0)
-
-    height = img.shape[0]  # shape[0] outputs height.
-    
-
-    width = img.shape[1] # shape[1] outputs width.
-    matrix_R = np.zeros((height, width))
-
-    # Step 1 - Calculate the x e y image derivatives (dx e dy)
-    dx = cv2.Sobel(img_gaussian, cv2.CV_64F, 1, 0, ksize=3)
-    dy = cv2.Sobel(img_gaussian, cv2.CV_64F, 0, 1, ksize=3)
-    # dy, dx = np.gradient(gray)
-
-    #   Step 2 - Calculate product and second derivatives (dx2, dy2 e dxy)
-    dx2 = np.square(dx)
-    dy2 = np.square(dy)
-    dxy = dx*dy
-
-    offset = int(window_size / 2)
-    #   Step 3 - Calcular a soma dos produtos das derivadas para cada pixel (Sx2, Sy2 e Sxy)
-
-    for y in range(offset, height-offset):
-        for x in range(offset, width-offset):
-            Sx2 = np.sum(dx2[y-offset:y+1+offset, x-offset:x+1+offset])
-            Sy2 = np.sum(dy2[y-offset:y+1+offset, x-offset:x+1+offset])
-            Sxy = np.sum(dxy[y-offset:y+1+offset, x-offset:x+1+offset])
-
-            #   Step 4 - Define the matrix H(x,y)=[[Sx2,Sxy],[Sxy,Sy2]]
-            H = np.array([[Sx2, Sxy], [Sxy, Sy2]])
-
-            #   Step 5 - Calculate the response function ( R=det(H)-k(Trace(H))^2 )
-            det = np.linalg.det(H)
-            tr = np.matrix.trace(H)
-            R = det-k*(tr**2)
-            matrix_R[y-offset, x-offset] = R
-
-    #   Step 6 - Apply a threshold
-    cv2.normalize(matrix_R, matrix_R, 0, 1, cv2.NORM_MINMAX)
-    for y in range(offset, height-offset):
-        for x in range(offset, width-offset):
-            value = matrix_R[y, x]
-            if value > threshold:
-                # cornerList.append([x, y, value])
-                cv2.circle(img, (x, y), 3, (0, 255, 0))
-
-    return img
+    """
+    if isinstance(array, list):
+        return list(map(normalize, array,newMax,newMin))
+    if isinstance(array, tuple):
+        return tuple(normalize(list(array),newMax,newMin))
+    normalizedData = (array-np.min(array))/(np.max(array)-np.min(array))*(newMax-newMin) + newMin
+    return normalizedData
 
 
